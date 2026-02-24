51ec8a18ec48eba3ed2f4e8814ae4954eba4c24b
Kazilsky <64886196+Kazilsky@users.noreply.github.com>
2026-02-24 19:22:12 +0500

    Create vite.config.js file

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  plugins: [svelte()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  server: {
+    strictPort: false,
+    middlewareMode: false,
+  },
+  build: {
+    outDir: 'dist',
+  },
+})
